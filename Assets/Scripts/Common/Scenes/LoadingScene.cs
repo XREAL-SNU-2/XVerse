8c78ac743fb67afaeb17b6c1d261d2bd4a6418a0
Sung Hoon Bae <70627437+MondaySHB@users.noreply.github.com>
2022-07-15 04:43:24 +0900

    [feat] Scene 흐름을 위한 LoadingScene 구성
    Login -> Loading -> Login(뒤로 가기) or PersonalWorld
    PersonalWorld -> Loading -> PersonalWorld(뒤로 가기) or OpenSpace
    OpenSpace -> Loading -> OpenSpace(뒤로 가기)
    씬 흐름 구성

--- Assets/Scripts/Common/Scenes/LoadingScene.cs
+++ Assets/Scripts/Common/Scenes/LoadingScene.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Xverse.Scene;
 
 public class LoadingScene : BaseScene
 {
+    public Button LoginButton;
+    public Button OpenSpaceButton;
+    public Button PersonalWorldButton;
     protected override void Init()
     {
         base.Init();
+
         SceneType = Xverse.Scene.Scene.Loading;
+        if(SceneManagerEx.Instance.PrevScene.Equals(Scene.Login))
+        {
+            LoginButton.gameObject.SetActive(true);
+            PersonalWorldButton.gameObject.SetActive(true);
+        }
+        if (SceneManagerEx.Instance.PrevScene.Equals(Scene.OpenSpace))
+        {
+            OpenSpaceButton.gameObject.SetActive(true);
+        }
+        if (SceneManagerEx.Instance.PrevScene.Equals(Scene.PersonalWorld))
+        {
+            PersonalWorldButton.gameObject.SetActive(true);
+            OpenSpaceButton.gameObject.SetActive(true);
+        }
+
     }
     public override void Clear()
     {
 
+    }
+
+    public void ToLogin()
+    {
+        SceneManagerEx.Instance.LoadScene(Xverse.Scene.Scene.Login);
     }
     public void ToOpenSpace()
     {
